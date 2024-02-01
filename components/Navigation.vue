fbb221b2325e3dba9f88c9c38f8d67ab3a426ada
Jozef Janecek <jozef.janecek@asseco-ceit.com>
2024-02-01 19:59:07 +0100

    navigation visual hotfix

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,5 +1,5 @@
 <template>
-  <UHorizontalNavigation :ui="{wrapper: 'justify-center fixed xl:relative inset-x-0 bottom-0 z-10 bg-gray-950 border-t border-indigo-950', base: 'text-lg rounded-xl  items-center' }" :links="links"  />
+  <UHorizontalNavigation :ui="{wrapper: 'justify-center fixed xl:relative inset-x-0 xl:border-t-0 bg-gray-50 xl:bg-transparent bottom-0 z-10 dark:bg-gray-950 border-t border-indigo-50 dark:border-indigo-950', base: 'text-lg rounded-xl  items-center' }" :links="links"  />
 </template>
 
 
